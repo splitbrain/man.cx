02ca1b2c203b1b1b7d18bcc236a7989ae0d40675
Andreas Gohr <andi@splitbrain.org>
2018-04-20 14:46:04 +0200

    updated google analytics

--- src/tpl/scripts.php
+++ src/tpl/scripts.php
@@ -1,12 +1,12 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-83791-7"></script>
 <script>
     // google analytics
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-83791-7', 'auto');
-    ga('send', 'pageview');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-83791-7', { 'anonymize_ip': true });
 
     // adsense - one push for every ad
     var adcount = document.getElementsByClassName('adsbygoogle').length;
@@ -24,7 +24,6 @@
             } else {
                 this.nextElementSibling.style.display = 'none';
             }
-            
         });
     });
 
